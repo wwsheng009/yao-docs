1825fbf00e9ac87d53dce8a92e86690f68b99b01
wwsheng009 <vincentwwsheng@gmail.com>
2023-04-01 12:18:38 +0800

    update algolia searchParameters

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -2,7 +2,10 @@ export default {
   appId: 'Z0GOIBJSIG',
   apiKey: 'f3f332389d40f82741dc99678e577e5d',
   indexName: 'yao-docs',
-  facets: ['*'],
+  searchParameters: {
+    facets: ['*'],
+  },
+
   locales: {
     zh: {
       placeholder: '搜索文档',
